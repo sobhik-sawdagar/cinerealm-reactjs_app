6a3fcb573365dfaa72b6c8e1c75c5960d11d3c69
sobhik-sawdagar <sobhiksawdagar@gmail.com>
2023-08-30 10:55:34 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => 
+{
+   return (
+    <div>
+     <h1>
+        Welcome to CineRealm Movie App
+     </h1>
+   </div>
+   );
+};
+
+export default App;
